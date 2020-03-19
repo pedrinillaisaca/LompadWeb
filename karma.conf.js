511c363175a09876dd99182c0804cffa92951f2a
yigitfindikli <yigitfindikli@fd9223c1-c77d-41e1-af93-17165896985c>
2020-03-19 09:05:04 +0000

    Karma fix

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, './coverage/sapphire'),
+            dir: require('path').join(__dirname, './coverage/mirage'),
             reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true
         },
